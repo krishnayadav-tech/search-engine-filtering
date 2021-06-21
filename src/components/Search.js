cb3106c0e461fc72f9745b3b2a673bfee26388ec
something <yadav_krishna@outlook.in>
2021-06-21 11:06:33 +0530

    added responsiveness

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,6 @@ import FilterForm from './FilterForm';
 export default class Search extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       priceFrom: '',
       priceTo: '',
